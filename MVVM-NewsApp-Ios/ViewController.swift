b36000f06a0bc96d6231b3814e7e62892d7a05f7
dev-aldy <aldy.ramadhan012@gmail.com>
2020-07-23 00:28:54 +0700

    Initial Commit

---
+++ MVVM-NewsApp-Ios/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MVVM-NewsApp-Ios
+//
+//  Created by iMac-Appschef on 23/07/20.
+//  Copyright © 2020 iMac-Appschef. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ MVVM-NewsApp-IosTests/MVVM_NewsApp_IosTests.swift
@@ -0,0 +1,34 @@
+//
+//  MVVM_NewsApp_IosTests.swift
+//  MVVM-NewsApp-IosTests
+//
+//  Created by iMac-Appschef on 23/07/20.
+//  Copyright © 2020 iMac-Appschef. All rights reserved.
+//
+
+import XCTest
+@testable import MVVM_NewsApp_Ios
+
+class MVVM_NewsApp_IosTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
